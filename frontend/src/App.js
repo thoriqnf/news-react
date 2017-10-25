e14c3b33b5a817552edcd87d5ea47147144429a2
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-25 17:11:52 +0700

    change structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import list from './list';
-import { Grid, Row, FormGroup } from 'react-bootstrap';
+import {Grid, Row, FormGroup} from 'react-bootstrap';
 
-// filter the results by search
-function isSearched(searchTerm){
-  return function(item){
+// function search
+function isSearched(searchTerm) {
+  return function(item) {
     return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
   }
 }
 
 class App extends Component {
 
-  // setting up internal component state
-  // ES6 class can use constructor to initialize internal state
-  constructor(props){
-    // super props sets this.props to the constructor
+
+  constructor(props) {
     super(props);
 
-    // setting up state
+    // ngambil list dari list.js
     this.state = {
       list,
       searchTerm: ''
@@ -28,23 +26,22 @@ class App extends Component {
     this.searchValue = this.searchValue.bind(this);
   }
 
- 
- // lets rewrite removeItem function in ES6
- removeItem(id){
-  // const isNotId = item => item.objectID !== id;
-  const updatedList = this.state.list.filter(item => item.objectID !== id);
-  this.setState({ list: updatedList });
- }
+  // remove
+  removeItem(id) {
+    // const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
 
- // get input field value from search form
- searchValue(event){
-  // console.log(event)
-  this.setState({ searchTerm: event.target.value });
- }
+  // get input field value from search form
+  searchValue(event) {
+    // console.log(event)
+    this.setState({searchTerm: event.target.value});
+  }
 
   render() {
 
-    const { list, searchTerm } = this.state;
+    const {list, searchTerm} = this.state;
 
     console.log(this);
 
@@ -55,105 +52,86 @@ class App extends Component {
           <Row>
             <div className="jumbotron text-center">
 
-            <Search
-              onChange={ this.searchValue } 
-              value={ searchTerm }
-            >NEWSAPP</Search>
+              <Search onChange={this.searchValue} value={searchTerm}>NEWSAPP</Search>
 
             </div>
           </Row>
         </Grid>
 
-        <Table 
-          list={ list }
-          searchTerm={ searchTerm }
-          removeItem={ this.removeItem }
-        />
+        <Table list={list} searchTerm={searchTerm} removeItem={this.removeItem}/>
 
       </div>
     );
   }
 }
 
-
-const Search = ({ onChange, value, children }) => {
-  return(
-      <form>
+const Search = ({onChange, value, children}) => {
+  return (
+    <form>
       <FormGroup>
 
-        <h1 style={{ fontWeight: 'bold' }}>{ children }</h1> 
-        <hr style={{ border: '2px solid black', width: '100px' }} />
+        <h1 style={{
+          fontWeight: 'bold'
+        }}>{children}</h1>
+        <hr style={{
+          border: '2px solid black',
+          width: '100px'
+        }}/>
 
         <div className="input-group">
 
-        <input
-          className="form-control width100 searchForm" 
-          type="text" 
-          onChange={ onChange } 
-          value={ value } 
-        />
+          <input className="form-control width100 searchForm" type="text" onChange={onChange} value={value}/>
 
-        <span className="input-group-btn">
-          <button
-            className="btn btn-primary searchBtn"
-            type="submit"
-          >
-            Search
-          </button>
-        </span>
+          <span className="input-group-btn">
+            <button className="btn btn-primary searchBtn" type="submit">
+              Search
+            </button>
+          </span>
 
         </div>
 
-        </FormGroup>
-      </form>
-    )
+      </FormGroup>
+    </form>
+  )
 }
 
+const Table = ({list, searchTerm, removeItem}) => {
+  return (
+    <div className="col-sm-10 col-sm-offset-1">
+      {list.filter(isSearched(searchTerm)).map(item =>
+        <div key={item.objectID}>
+          <h1>
+            <a href={item.url}>
+              {item.title}</a>
+          </h1>
+          <h4>
 
-const Table = ({ list, searchTerm, removeItem }) => {
-  return(
-      <div className="col-sm-10 col-sm-offset-1">
-        {
-            list.filter( isSearched(searchTerm) ).map(item =>   
-              <div key={ item.objectID }>
-                <h1>
-                  <a href={ item.url }>
-                    { item.title }</a> 
-                </h1>
-                <h4>
+            {item.author}
+            | {item.num_comments}
+            Comments | {item.points}
+            Points
 
-                  { item.author } | { item.num_comments } Comments | { item.points } Points
-                
-                <Button
-                  className="btn btn-xs"
-                  type="button"
-                  onClick={ () => removeItem(item.objectID) }>
-                    Remove
-                </Button>
+            <Button className="btn btn-xs" type="button" onClick={() => removeItem(item.objectID)}>
+              Remove
+            </Button>
 
-                </h4> <hr />
+          </h4>
+          <hr/>
 
-              </div>
-            )
-          }
-      </div>
-    )
+      </div>)
+}
+    </div>
+  )
 }
 
-
-const Button = ({ onClick, children, className='' }) => 
-  <button
-    className={ className }  
-    onClick={ onClick } >
-    { children }
+const Button = ({
+  onClick,
+  children,
+  className = ''}) =>
+  <button className={className} onClick={onClick}>
+    {
+      children
+    }
   </button>
 
-
-
-
 export default App;
-
-
-
-
-
--- frontend/src/list.js
+++ frontend/src/list.js
@@ -2,24 +2,24 @@
 
 const list = [
   {
-    title: 'Tell me the truth without words',
-    url: 'http://kaloraat.com',
+    title: 'google',
+    url: 'http://google.com',
     author: 'Ryan',
     num_comments: 100,
     points: 50,
     objectID: 1
   },
   {
-    title: 'Oh no! The candle is out',
-    url: 'http://kaloraat.com',
+    title: 'facebook',
+    url: 'http://facebook.com',
     author: 'Zen',
     num_comments: 50,
     points: 20,
     objectID: 2
   },
   {
-    title: 'The hard earned black belt',
-    url: 'http://kaloraat.com',
+    title: 'yahoo',
+    url: 'http://yahoo.com',
     author: 'Ninja',
     num_comments: 10,
     points: 5,
